de68532f389b1e642d176994a6c8612839f1c379
Farukhrana14 <farukhrana14@gmail.com>
2021-03-26 19:02:22 +0600

    module 45 - node Express to html done

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
-
+const cors = require ('cors');
 
 
 const app = express();
 
+app.use(cors());
+//Sample Users data for testing Dynamic route 
+const users = ['Asad', 'Jafar', 'Iqbal', 'Moin', 'Parvin', 'Shabnoor', 'Mithila'];
 
-app.get('/', (req, res)=> {res.send ('Thank you for calling me')});
+
+//Get root route
+app.get('/', (req, res)=> {
+    const fruit = {
+        product: 'Orange',
+        price: 220
+    }
+
+    res.send (fruit);
+});
+
+
+// dynamic route for localhost:3000/users/:id
+// Use of req.params tells us what request parameter is sent to server >> req.params.id
+// req.query gives the query sent to server 
+app.get('/users/:id', (req, res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+})
+
+// parse body json
+app.use(express.json());
+
+// Post route
+app.post ('/addUser', (req, res)=> {
+    //Save to database, for now do this
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
+
+
 
 
 app.listen(3000, () => console.log('Listening to port 3000'));
